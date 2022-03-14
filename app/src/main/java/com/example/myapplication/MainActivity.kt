21f5a17082a40a29300ce429444dcaf8c27532e2
UrWallace <l18121495@morelia.tecnm.mx>
2022-03-13 23:09:23 -0600

    more changes to run my app xddd

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,10 +24,5 @@ private lateinit var binding: ActivityMainBinding
 
 
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-
         }
     }
